90b08d02fbcc1817746813f7dbb32b51b9dbb1fd
radwan-77 <radwantareq66@gmail.com>
2024-02-24 18:39:21 -0800

    create new widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,12 @@ import 'package:maharah_course_project/presentation/app.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class Name extends StatelessWidget {
+  const Name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
